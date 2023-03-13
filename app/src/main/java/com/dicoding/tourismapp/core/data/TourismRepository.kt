17e989eace58f790845e698488a11a68cbe6347b
rickyandrean99 <84983109+rickyandrean99@users.noreply.github.com>
2023-03-13 22:40:26 +0700

    Setup Domain Layer

--- app/src/main/java/com/dicoding/tourismapp/core/data/TourismRepository.kt
+++ app/src/main/java/com/dicoding/tourismapp/core/data/TourismRepository.kt
@@ -8,6 +8,7 @@ import com.dicoding.tourismapp.core.data.source.local.entity.TourismEntity
 import com.dicoding.tourismapp.core.data.source.remote.RemoteDataSource
 import com.dicoding.tourismapp.core.data.source.remote.response.TourismResponse
 import com.dicoding.tourismapp.core.domain.model.Tourism
+import com.dicoding.tourismapp.core.domain.repository.ITourismRepository
 import com.dicoding.tourismapp.core.utils.AppExecutors
 import com.dicoding.tourismapp.core.utils.DataMapper
 
@@ -15,8 +16,7 @@ class TourismRepository private constructor(
     private val remoteDataSource: RemoteDataSource,
     private val localDataSource: LocalDataSource,
     private val appExecutors: AppExecutors
-) {
-
+): ITourismRepository {
     companion object {
         @Volatile
         private var instance: TourismRepository? = null
@@ -31,7 +31,7 @@ class TourismRepository private constructor(
             }
     }
 
-    fun getAllTourism(): LiveData<Resource<List<Tourism>>> =
+    override fun getAllTourism(): LiveData<Resource<List<Tourism>>> =
         object : NetworkBoundResource<List<Tourism>, List<TourismResponse>>(appExecutors) {
             override fun loadFromDB(): LiveData<List<Tourism>> {
                 return Transformations.map(localDataSource.getAllTourism()) {
@@ -51,13 +51,13 @@ class TourismRepository private constructor(
             }
         }.asLiveData()
 
-    fun getFavoriteTourism(): LiveData<List<Tourism>> {
+    override fun getFavoriteTourism(): LiveData<List<Tourism>> {
         return Transformations.map(localDataSource.getFavoriteTourism()) {
             DataMapper.mapEntitiesToDomain(it)
         }
     }
 
-    fun setFavoriteTourism(tourism: Tourism, state: Boolean) {
+    override fun setFavoriteTourism(tourism: Tourism, state: Boolean) {
         val tourismEntity = DataMapper.mapDomainToEntity(tourism)
         appExecutors.diskIO().execute { localDataSource.setFavoriteTourism(tourismEntity, state) }
     }
